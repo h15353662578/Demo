6683496ae0fd9a870b682d4092b5874a87824838
Huasheng <h1785222224@qq.com>
2021-03-02 18:58:13 +0800

    1

--- demo-user-8000/src/main/java/com/hs/user/UserApplication.java
+++ demo-user-8000/src/main/java/com/hs/user/UserApplication.java
@@ -1,10 +1,8 @@
 package com.hs.user;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan({"com.hs.user.mapper"})
 @SpringBootApplication
 /***
  * @author 华生
---
+++ gateway-7000/src/main/java/com/hs/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.hs.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Auther: 华生
+ * @Date: 2021/03/02/15:38
+ * @Description:
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
